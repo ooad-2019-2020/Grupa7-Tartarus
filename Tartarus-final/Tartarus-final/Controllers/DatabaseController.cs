804b9b0932da37cbb7b27a9e4e661209e4cf1f26
Ajdin Hadzic <ajdinh1997@gmail.com>
2020-06-06 01:33:15 +0200

    Added Database section

---
+++ Tartarus-final/Tartarus-final/Controllers/DatabaseController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Tartarus_final.Models;
+
+namespace Tartarus_final.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public DatabaseController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
