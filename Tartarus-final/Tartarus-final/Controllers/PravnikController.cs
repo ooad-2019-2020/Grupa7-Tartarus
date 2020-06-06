ee508bddc396a094c97c2ee46e8695976b51bd98
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-06-06 18:22:17 +0200

    Added controllers & views for Upravnik, Cuvar & Pravnik

---
+++ Tartarus-final/Tartarus-final/Controllers/PravnikController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tartarus_final.Controllers
+{
+    public class PravnikController : Controller
+    {
+        [Authorize(Roles = "Pravnik")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tartarus-final/Tartarus-final/Controllers/UpravnikController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tartarus_final.Controllers
+{
+    public class UpravnikController : Controller
+    {
+        [Authorize(Roles = "Upravnik")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
