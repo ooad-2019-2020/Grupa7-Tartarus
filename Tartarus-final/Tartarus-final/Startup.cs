466027767ba80ec639a776c96e903ce6f99f2343
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-06-06 17:30:17 +0200

    Updated StartUp

--- Tartarus-final/Tartarus-final/Startup.cs
+++ Tartarus-final/Tartarus-final/Startup.cs
@@ -35,6 +35,7 @@ namespace Tartarus_final
               options.UseSqlServer(
                   Configuration.GetConnectionString("DbConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
