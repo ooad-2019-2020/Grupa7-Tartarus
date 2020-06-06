7f3678e2669338fd2b25307e42322b4061078d76
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-06-06 14:19:04 +0200

    Updated NasContext

--- Tartarus-final/Tartarus-final/Models/NasContext.cs
+++ Tartarus-final/Tartarus-final/Models/NasContext.cs
@@ -17,12 +17,14 @@ namespace Tartarus_final.Models
    
         public DbSet<Obavijest> Obavijest { get; set; }
         public DbSet<Poruka> Poruka { get; set; }
-
+        public DbSet<Zatvorenik> Zatvorenik { get; set; }
+       
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Poruka>().ToTable("Poruke");
             modelBuilder.Entity<Obavijest>().ToTable("Obavijesti");
+            modelBuilder.Entity<Zatvorenik>().ToTable("Zatvorenik");
         }
 
     }
