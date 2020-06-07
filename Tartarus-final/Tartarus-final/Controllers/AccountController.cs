66e05e930d68f22cf6457c9f453e5142f385190e
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-06-07 17:52:32 +0200

    Updated register form

--- Tartarus-final/Tartarus-final/Controllers/AccountController.cs
+++ Tartarus-final/Tartarus-final/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace Tartarus_final.Controllers
 {
     public class AccountController : Controller
