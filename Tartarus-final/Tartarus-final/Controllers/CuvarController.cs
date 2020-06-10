459ed33d7423118f39b9503a33952495785094ec
Faris Mehmedovic <fmehmedovi1@etf.unsa.ba>
2020-06-10 04:43:07 +0200

    Updated authentication

--- Tartarus-final/Tartarus-final/Controllers/CuvarController.cs
+++ Tartarus-final/Tartarus-final/Controllers/CuvarController.cs
@@ -2,16 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Tartarus_final.Models;
 
 namespace Tartarus_final.Controllers
 {
     public class CuvarController : Controller
     {
-        [Authorize(Roles = "Cuvar")]
-        public IActionResult Index()
+
+        private readonly NasContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public CuvarController(NasContext context, UserManager<ApplicationUser> userManager)
         {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ApplicationUser applicationUser = await _userManager.GetUserAsync(User);
+
+            if (RegistrationTypes.Cuvar != applicationUser?.RegistrationType || applicationUser?.RegistrationType == null)
+            {
+                return View("~/Views/Home/Index.cshtml");
+            }
             return View();
         }
     }
--- Tartarus-final/Tartarus-final/Controllers/UpravnikController.cs
+++ Tartarus-final/Tartarus-final/Controllers/UpravnikController.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Tartarus_final.Models;
 
 namespace Tartarus_final.Controllers
 {
     public class UpravnikController : Controller
     {
-        [Authorize(Roles = "Upravnik")]
-        public IActionResult Index()
+        private readonly NasContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UpravnikController(NasContext context, UserManager<ApplicationUser> userManager)
         {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ApplicationUser applicationUser = await _userManager.GetUserAsync(User);
+
+            if (RegistrationTypes.Upravnik != applicationUser?.RegistrationType || applicationUser?.RegistrationType == null)
+            {
+                return View("~/Views/Home/Index.cshtml");
+            }
             return View();
         }
     }
